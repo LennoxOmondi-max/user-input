8ab522397b05340a06f75f026a301f708c259547
Lennox Omondi <fut.lennoxomondi@gmail.com>
2024-02-23 14:26:20 +0300

    Add user_input.py file

---
+++ user_input.py
@@ -0,0 +1,4 @@
+name = input("What is your name? ")
+age = input("What is your age? ")
+location = input("What is your current location? ")
+print("Hello there " + name + "!", "I can't believe you're " + age + " years old and live in " + location + " just like me :)" )
